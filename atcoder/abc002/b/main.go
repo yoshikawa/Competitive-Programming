740ce938f26283ffc2c8a6db7998d03ad2f0c7e0
yoshikawataiki <yoshikawataiki@gmail.com>
2021-01-05 22:20:11 +0900

    update

--- atcoder/abc002/b/main.go
+++ atcoder/abc002/b/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var x string
+	fmt.Scan(&x)
+
+	for i := 0; i < len(x); i++ {
+		s := x[i]
+		if (s != 'a') && (s != 'i') && (s != 'u') && (s != 'e') && (s != 'o') {
+			fmt.Printf("%c", s)
+		}
+	}
+	fmt.Println("")
 }
